4678ebe16f58fa6ae61ecdfabb5471df9068f308
DootLord <shaunoman840@gmail.com>
2025-01-28 20:22:02 +0000

    a

--- server.js
+++ server.js
@@ -1,27 +1,17 @@
 const fs = require('fs');
-const http = require('http');
 const https = require('https');
-
 const express = require('express');
 const path = require('path');
 const app = express();
+
 const certs = {
   key: fs.readFileSync('/etc/letsencrypt/live/dootlord.meme/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/dootlord.meme/cert.pem')
 }
 
-// app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, 'static')));
 
-app.get("/", (req, res) => {
-  res.send("ayo whats up man");
-});
-
-// const httpServer = http.createServer(app);
 const httpsServer = https.createServer(certs, app);
-
-// httpServer.listen(80, () => {
-//   console.log('HTTPS Server running on port 80');
-// });
 
 httpsServer.listen(443, (err) => {
   if (err) {
