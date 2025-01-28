eddbeff966949cfa6e12d59701fac3d2ac5a8aed
DootLord <shaunoman840@gmail.com>
2025-01-28 20:21:28 +0000

    a

--- server.js
+++ server.js
@@ -16,18 +16,18 @@ app.get("/", (req, res) => {
   res.send("ayo whats up man");
 });
 
-const httpServer = http.createServer(app);
+// const httpServer = http.createServer(app);
 const httpsServer = https.createServer(certs, app);
 
-httpServer.listen(80, () => {
-  console.log('HTTPS Server running on port 80');
-});
-
-// httpsServer.listen(443, (err) => {
-//   if (err) {
-//     console.error('Failed to start HTTPS Server:', err);
-//   } else {
-//     console.log('HTTPS Server running on port 443');
-//   }
+// httpServer.listen(80, () => {
+//   console.log('HTTPS Server running on port 80');
 // });
+
+httpsServer.listen(443, (err) => {
+  if (err) {
+    console.error('Failed to start HTTPS Server:', err);
+  } else {
+    console.log('HTTPS Server running on port 443');
+  }
+});
 
