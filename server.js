443d087743a2784b396b3743b4e47a05b4b7d972
DootLord <shaunoman840@gmail.com>
2025-01-28 16:52:08 +0000

    Set port to 80 (Via a commit i know it's wrong!)

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 4168;
+const port = 80;
 
 app.use(express.static(path.join(__dirname, 'static')));
 
