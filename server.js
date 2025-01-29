054f92392205edd7ea186c64b90750f562b756c8
DootLord <shaunoman840@gmail.com>
2025-01-29 01:06:10 +0000

    re-enable https

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 const http = require("http");
-// const https = require('https');
+const https = require('https');
 const express = require('express');
 const path = require('path');
 const app = express();
 
-// const certs = {
-//   key: fs.readFileSync('/etc/letsencrypt/live/dootlord.meme/privkey.pem'),
-//   cert: fs.readFileSync('/etc/letsencrypt/live/dootlord.meme/cert.pem')
-// }
+const certs = {
+  key: fs.readFileSync('/etc/letsencrypt/live/dootlord.meme/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/dootlord.meme/cert.pem')
+}
 
 app.use(express.static(path.join(__dirname, 'static')));
 
-// const httpsServer = https.createServer(certs, app);
+const httpsServer = https.createServer(certs, app);
 const httpServer = http.createServer(app);
 
 httpServer.listen(80, (err) => {
@@ -22,11 +22,11 @@ httpServer.listen(80, (err) => {
     console.log('HTTP Server running on port 80');
   }
 })
-// httpsServer.listen(443, (err) => {
-//   if (err) {
-//     console.error('Failed to start HTTPS Server:', err);
-//   } else {
-//     console.log('HTTPS Server running on port 443');
-//   }
-// });
+httpsServer.listen(443, (err) => {
+  if (err) {
+    console.error('Failed to start HTTPS Server:', err);
+  } else {
+    console.log('HTTPS Server running on port 443');
+  }
+});
 
